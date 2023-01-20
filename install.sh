98da5776be0556f57df9cc5bd44868451b9f5be2
Josh Dirkx <joshdirkx@gmail.com>
2023-01-20 07:44:48 -0800

    add editorconfig

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 if ! command -v brew &> /dev/null
 then
-	echo "Installing Homebrew"
-  	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "Installing Homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 echo "Installing GitHub"
