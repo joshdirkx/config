e03d17598c7ebeff3dd04b456b299e64f3835473
Josh Dirkx <joshdirkx@gmail.com>
2023-01-19 19:32:28 -0800

    some basics

---
+++ Brewfile
@@ -0,0 +1,14 @@
+brew "git"
+brew "neovim"
+brew "fzf"
+brew "tree-sitter"
+brew "redis"
+brew "postgresql"
+brew "elasticsearch"
+brew "npm"
+brew "alacritty"
+
+tap "homebrew/cask-fonts"
+cask "font-fira-code"
+
+cask "slack"
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/user/bin/env bash
+
+if ! command -v brew &> /dev/null
+then
+	echo "Installing Homebrew"
+  	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+echo "Installing GitHub"
+brew install gh
+
+echo "Authenticating with GitHub"
+gh auth login
+
+echo "Running brew bundle"
+brew bundle --file="./Brewfile"
+
