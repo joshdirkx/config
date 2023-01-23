b9baa66c369848ba171276facaebe1387f431c81
Josh Dirkx <joshdirkx@gmail.com>
2023-01-22 20:45:53 -0800

    remove arrow keys

--- nvim/init.lua
+++ nvim/init.lua
@@ -125,6 +125,16 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 -- [[ Setting options ]]
 -- See `:help vim.o`
 
+-- no arrow keys
+vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Up>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Down>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Left>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Right>', '<Nop>', { noremap = true, silent = true })
+
 -- <esc> -> jj
 vim.api.nvim_set_keymap('i', 'jj', '<esc>', { noremap = true, silent = true })
 -- removes <esc> keybinding
