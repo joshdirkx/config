47442a8c4b62f3966bd28c9baf5d0a8f776408e1
Josh Dirkx <joshdirkx@gmail.com>
2023-01-22 18:38:14 -0800

    fix bindings

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,10 @@ if status is-interactive
     alias ll "exa -l -g --icons"
     alias lla "ll -a"
   end
+
+  fzf_configure_bindings --directory=\cf --git_log=\cl --git_status=\cs --processes=\cp
+  set fzf_preview_dir_command exa --all -color=always
 end
 
 starship init fish | source
 
-set fzf_preview_dir_cmd exa --all --color=always
---
+++ fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,4 @@
+function fish_user_key_bindings
+    bind --erase \cl
+  
+end
